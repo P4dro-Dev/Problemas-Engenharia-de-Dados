6e15c9abd9e48c1df0c704446aaaa01b55d62acd
Pedro Henrique <Ph9084533@gmail.com>
2024-12-31 17:28:21 -0300

    Add files via upload
    Adicionando a questão III

---
+++ Problemas_Engenharia-de-Dados-0_1/III.py
@@ -0,0 +1,6 @@
+# Questão 3: Encontrar o valor mínimo em uma lista de números.
+def valor_minimo(numeros):
+    return min(numeros)
+
+numeros = [10, 20, 30, 40, 50]
+print(f"O valor mínimo é: {valor_minimo(numeros)}")
