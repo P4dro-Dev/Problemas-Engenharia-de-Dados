a659d768056f2008a99dc681b4bec9b16956abac
Pedro Henrique <Ph9084533@gmail.com>
2024-12-31 17:27:24 -0300

    Add files via upload
    Adicionando a questão II a pasta!

---
+++ Problemas_Engenharia-de-Dados-0_1/II.py
@@ -0,0 +1,6 @@
+# Questão 2: Encontrar o valor máximo em uma lista de números.
+def valor_maximo(numeros):
+    return max(numeros)
+
+numeros = [10, 20, 30, 40, 50]
+print(f"O valor máximo é: {valor_maximo(numeros)}")
